0c201ded6890dbd738013ab525556c470d474c0b
andrew nevins <andrew@red-robot.co.uk>
2013-04-17 02:09:03 +0100

    organised resize image function and made it run only if there are new images

--- application/controllers/items.php
+++ application/controllers/items.php
@@ -35,42 +35,13 @@ class Items extends CI_Controller {
 
 	public function setItems() {
 		$items = simplexml_load_file( base_url() . 'assets/xml/portfolioitems.xml' );
-		$config[ 'image_library' ] = 'gd2';
-		$config[ 'create_thumb' ] = TRUE;
-		$config[ 'maintain_ratio' ] = TRUE;
-		$config[ 'width' ]	 = 252;
-		$config[ 'height' ]	= 243;
-
-		foreach ( $items as $item ) {
-
-			$image_path = "assets/i/items/$item->image_url";
-
-
-			if ( isset( $item->image_url ) ) {
-
-				$config[ 'source_image' ] =  $image_path;
-				$this->image_lib->initialize( $config );
-
-				if ( !$this->image_lib->resize() ) {
-					echo $this->image_lib->display_errors();
-				}
-
-				$name = str_replace('.png', '_thumb.png', $item->image_url);
-
-				$item->thumb_url = base_url() . 'assets/i/items/' . $name;
-
-			}
-
-		}
-
-		$this->portfoliomodel->setPortfolioItems($items);
+		$this->portfoliomodel->resizeImages( $items );
+		$this->portfoliomodel->setPortfolioItems( $items );
 	}
 
 	public function getCategoryItems ( $category ) {
-
 		$categoryItems = $this->portfoliomodel->getPortfolioItemsFromCategory( $category );
 		echo $categoryItems;
-
 	}
 
 }
--- application/models/portfoliomodel.php
+++ application/models/portfoliomodel.php
@@ -60,5 +60,46 @@ class Portfoliomodel extends CI_Model {
 
 	}
 
+	public function resizeImages( $items ) {
+
+		$config[ 'image_library' ] = 'gd2';
+		$config[ 'create_thumb' ] = TRUE;
+		$config[ 'maintain_ratio' ] = TRUE;
+		$config[ 'width' ]	 = 252;
+		$config[ 'height' ]	= 243;
+
+		foreach ( $items as $item ) {
+
+			$image_path = "assets/i/items/$item->image_url";
+			
+			$name = str_replace('.png', '_thumb.png', $item->image_url);
+			$thumb_file = 'assets/i/items/' . $name;
+
+			if ( !file_exists( $thumb_file ) ) {
+
+				if( isset( $item->image_url ) ) {
+
+					$config[ 'source_image' ] =  $image_path;
+					$this->image_lib->initialize( $config );
+
+					if ( !$this->image_lib->resize() ) {
+						echo $this->image_lib->display_errors();
+					}
+
+					$name = str_replace('.png', '_thumb.png', $item->image_url);
+					$thumb_file = base_url() . 'assets/i/items/' . $name;
+
+					$item->thumb_url = $thumb_file;
+				}
+				
+			}
+			else {
+				$item->thumb_url = base_url() . $thumb_file;
+			}
+
+		}
+		
+	}
+
 }
 ?>
